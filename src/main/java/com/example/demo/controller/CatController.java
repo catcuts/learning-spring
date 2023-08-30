25c29554676bed6a323a06ebbafb724ae66584a4
catcuts <354151733@qq.com>
2023-08-30 13:27:27 +0800

    feat: 增设一个获取指定Cat的REST-API

--- src/main/java/com/example/demo/controller/CatController.java
+++ src/main/java/com/example/demo/controller/CatController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +37,38 @@ public class CatController {
             0/*pageNo*/, 12/*pageSize*/, Sort.by("createdAt").descending()
         );
         return catRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cat> catById(@PathVariable("id") Long id) {
+        Optional<Cat> optCat = catRepo.findById(id);
+        // 若找到了 Cat 对象，则返回它，否则返回 404
+        if (optCat.isPresent()) {
+            return new ResponseEntity<>(optCat.get(), HttpStatus.OK/*200*/);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND/*404*/);
+        // 注：这里 return new ResponseEntity<>... 省略了泛型参数，因为它们可以从方法的返回值类型中推断出来，也就是 Cat。
+        //     也就是说，因为返回值已经指定为 ResponseEntity<Cat>，所以在返回的时候可以省略泛型参数，即 ResponseEntity<>。
+    }
+
+    // 这个接口保留用于示例，与 catById 相比，它的返回值类型为 Optional<Cat>，当找不到 Cat 对象时，返回值为 null 且响应码是 200 而不是 404。
+    @GetMapping("/_/{id}")
+    public Optional<Cat> _catById(@PathVariable("id") Long id) {
+        // 注：
+        //    这里使用 Optional<Ingredient> 作为返回值类型，
+        //    是为了避免返回 null 值，从而避免空指针异常（NullPointerException）。
+        //    它会强制调用者在使用返回值之前，先检查返回值是否为空或者做防止返回空值的处理，否则编译器会报错。
+        //    例如：
+        //      - 调用者可以在返回值上：
+        //        - 使用 orElse() 方法来在其为空时使用一个默认值；
+        //        - 使用 orElseThrow() 方法来在其为空时抛出一个异常；
+        //        - 使用 ifPresent() 方法来在其为非空时执行一个操作等。
+        //      - 调用者也可以在 Optional 类上：
+        //        - 使用 empty() 方法来创建一个空的 Optional 对象等。
+        //    这里无需自行处理空值，原因是：
+        //    因为 CatRepository 继承自 PagingAndSortingRepository 又继承自 CrudRepository， 
+        //    而 CrudRepository 的 findById() 方法返回的就是 Optional<T> 类型。
+        return catRepo.findById(id);
     }
 
 }
